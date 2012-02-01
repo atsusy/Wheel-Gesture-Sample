a77ba8b8ea1bbce527c664e1c212ed661f12b86c
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2012-02-01 09:39:52 +0900

    Fixed indent.

--- Resources/app.js
+++ Resources/app.js
@@ -21,21 +21,21 @@ var initialAngle = 0.0;
 var currentAngle = 0.0;
 
 wheelImage.addEventListener('touchstart', function(e){
-  initialAngle = Math.atan2(e.x - this.width/2.0, -(e.y - this.height/2.0));
+	initialAngle = Math.atan2(e.x - this.width/2.0, -(e.y - this.height/2.0));
 });
 
 wheelImage.addEventListener('touchmove', function(e){
-  var movedAngle = Math.atan2(e.x - this.width/2.0, -(e.y - this.height/2.0));
-  var deltaAngle = movedAngle - initialAngle;
-  currentAngle += deltaAngle;
-  if (currentAngle > 2.0 * Math.PI){
-  	currentAngle -= 2.0 * Math.PI; 
-  }
-  if (currentAngle < 0.0){
-  	currentAngle += 2.0 * Math.PI;
-  }
-  wheelImage.transform = Ti.UI.create2DMatrix().rotate(currentAngle / Math.PI * 180.0);
-  angleLabel.text = parseInt(currentAngle / Math.PI * 180.0);
+	var movedAngle = Math.atan2(e.x - this.width/2.0, -(e.y - this.height/2.0));
+	var deltaAngle = movedAngle - initialAngle;
+	currentAngle += deltaAngle;
+	if (currentAngle > 2.0 * Math.PI){
+		currentAngle -= 2.0 * Math.PI; 
+	}
+  	if (currentAngle < 0.0){
+  		currentAngle += 2.0 * Math.PI;
+  	}
+  	wheelImage.transform = Ti.UI.create2DMatrix().rotate(currentAngle / Math.PI * 180.0);
+  	angleLabel.text = parseInt(currentAngle / Math.PI * 180.0);
 });
 
 window.open();
